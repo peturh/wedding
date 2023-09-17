d151600dc471ecbca730f55def61c56afd3f095d
peturh <>
2023-09-17 14:33:05 +0200

    feat: working form

--- src/lib/components/OSA.svelte
+++ src/lib/components/OSA.svelte
@@ -1,7 +1,61 @@
 <script lang="ts">
+	import type { ActionData } from './$types';
+	export let form: ActionData;
 </script>
 
 <section>
 	<h2 id="osa">Om svar anhålles</h2>
-	<p>O.s.a senast 1 mars 2024 i formuläret nedan. Fyll gärna i eventuella allergier här.</p>
+	<p>O.s.a senast 1 mars 2024 i formuläret nedan.</p>
+
+	<form method="post">
+		{#if form?.missing}<p class="error">Missing field required!</p>{/if}
+		<div class="mb-4">
+			<label for="name">Namn</label>
+			<input name="name" placeholder="Namn" type="text" value={form?.name ?? ''} />
+		</div>
+		<div class="mb-4">
+			<label for="phoneNumber">Telefonnummer</label>
+
+			<input
+				name="phoneNumer"
+				placeholder="Telefonnummer"
+				type="text"
+				value={form?.phoneNumer ?? ''}
+			/>
+		</div>
+
+		<div class="mb-4">
+			<label for="email">Email</label>
+
+			<input name="email" placeholder="Email" type="email" value={form?.email ?? ''} />
+		</div>
+
+		<div class="mb-4">
+			<label for="fullVegetarian">Äter bara vegetariskt</label>
+
+			<input name="fullVegeterian" type="checkbox" value={form?.fullVegeterian ?? ''} />
+		</div>
+		<div class="mb-4">
+			<label for="foodPreference">Allergier eller matpreferenser</label>
+			<input name="foodPreference" type="text" value={form?.foodPreference ?? ''} />
+		</div>
+		<div class="mb-4">
+			<label for="password">Lösenord som finns i inbjudan</label>
+			<input name="password" type="password" value={form?.password ?? ''} />
+		</div>
+		<button class="send-response" type="submit">OSA</button>
+	</form>
 </section>
+
+<style>
+	.send-response {
+		width: 100%;
+		border: 1px solid black;
+		border-radius: 6px;
+		padding: 10px;
+		box-shadow: 1px 1px 1px black;
+		&:hover {
+			box-shadow: none;
+		}
+	}
+</style>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,32 @@
+import prisma from '$lib/prisma';
+import type { PageServerLoad } from './$types';
+
+
+import { fail } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions = {
+    // 1.
+    default: async ({ request }) => {
+        const data = await request.formData();
+        console.log(data)
+        let password = data.get('password')
+
+        let name = data.get("name") || ''
+        let phoneNumber = data.get("phoneNumber") || ''
+        let email = data.get("email") || ''
+        let fullVegeterian = data.get("fullVegeterian") || ''
+        let foodPreference = data.get("foodPreference") || ''
+        await prisma.guest.create({
+            data: {
+                name: name.toString(),
+                phoneNumber: phoneNumber.toString(),
+                "email": email.toString(),
+                "fullVegeterian": fullVegeterian ? true : false,
+                "foodPreference": foodPreference.toString()
+            }
+        });
+        throw redirect(303, `/`)
+    }
+} satisfies Actions;
