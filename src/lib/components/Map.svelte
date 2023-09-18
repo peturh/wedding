2d5da670e6147db8134b169ceea146416d1fdec1
peturh <>
2023-09-18 10:39:32 +0200

    fix: lazy load map

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+	let appended = false;
+	function actionWhenInViewport(e: any) {
+		const observer = new IntersectionObserver((entries) => {
+			console.log('in view', entries);
+			if (entries[0].isIntersecting && appended === false) {
+				let wrapper = document.getElementById('map-wrapper');
+				let iframe = document.createElement('iframe');
+				iframe.setAttribute('title', 'Karta över platser');
+				iframe.setAttribute(
+					'src',
+					'https://storage.googleapis.com/maps-solutions-u7luppubmm/locator-plus/7su7/locator-plus.html'
+				);
+				iframe.setAttribute('height', '100%');
+				iframe.setAttribute('width', '100%');
+				iframe.setAttribute('style', 'border:0');
+				iframe.setAttribute('loading', 'lazy');
+				wrapper?.append(iframe);
+				appended = true;
+			}
+		});
+
+		observer.observe(e);
+	}
 </script>
 
 <section>
 	<h2 id="karta">Hitta hit</h2>
-	<div class="map-wrapper">
-		<iframe
-			title="Karta över platser"
-			src="https://storage.googleapis.com/maps-solutions-u7luppubmm/locator-plus/7su7/locator-plus.html"
-			width="100%"
-			height="100%"
-			style="border:0;"
-			loading="lazy"
-		/>
-	</div>
+	<div id="map-wrapper" class="map-wrapper" use:actionWhenInViewport />
 </section>
 
 <style lang="less">
