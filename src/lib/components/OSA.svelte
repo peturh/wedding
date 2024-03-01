3492bf67523fb3ee69efcde723e82df45bef2b9f
Pétur G. Hjartarson <petur.hjartarson@puzzel.com>
2024-03-01 21:18:41 +0100

    feat: remove rsvp on second of march

--- src/lib/components/OSA.svelte
+++ src/lib/components/OSA.svelte
@@ -3,82 +3,94 @@
 
 	import { enhance } from '$app/forms';
 	import type { ActionData } from './$types';
+	import dayjs from 'dayjs';
 	export let form: ActionData;
+	const now = dayjs();
+	const secondOfMarch = dayjs('2024-03-02');
+	export let openRSVP = now.diff(secondOfMarch) < 0 ? true : false;
 </script>
 
-<section>
-	<h2 id="osa">Om svar anhålles</h2>
-	<p>O.s.a senast första mars 2024.</p>
-	<p>En person per formulär.</p>
+<h2 id="osa">Om svar anhålles</h2>
 
-	<form method="post" use:enhance>
-		<div class="mb-4">
-			<label for="name">För- och efternamn</label>
-			<input
-				id="name"
-				name="name"
-				placeholder="För- och efternamn"
-				type="text"
-				value={form?.name ?? ''}
-				required
-			/>
-		</div>
-		<div class="mb-4">
-			<label for="phoneNumber">Telefonnummer</label>
+{#if openRSVP}
+	<section>
+		<p>O.s.a senast första mars 2024.</p>
+		<p>En person per formulär.</p>
 
-			<input
-				id="phoneNumber"
-				name="phoneNumber"
-				placeholder="Telefonnummer"
-				type="text"
-				value={form?.phoneNumber ?? ''}
-				required
-			/>
-		</div>
+		<form method="post" use:enhance>
+			<div class="mb-4">
+				<label for="name">För- och efternamn</label>
+				<input
+					id="name"
+					name="name"
+					placeholder="För- och efternamn"
+					type="text"
+					value={form?.name ?? ''}
+					required
+				/>
+			</div>
+			<div class="mb-4">
+				<label for="phoneNumber">Telefonnummer</label>
 
-		<div class="mb-4">
-			<label for="email">Email</label>
+				<input
+					id="phoneNumber"
+					name="phoneNumber"
+					placeholder="Telefonnummer"
+					type="text"
+					value={form?.phoneNumber ?? ''}
+					required
+				/>
+			</div>
 
-			<input
-				id="email"
-				name="email"
-				placeholder="Email"
-				type="email"
-				value={form?.email ?? ''}
-				required
-			/>
-		</div>
+			<div class="mb-4">
+				<label for="email">Email</label>
 
-		<div class="mb-4">
-			<label for="foodPreference">Allergier eller matpreferenser</label>
-			<input
-				id="foodPreference"
-				placeholder="Allergier eller matpreferenser"
-				name="foodPreference"
-				type="text"
-				value={form?.foodPreference ?? ''}
-			/>
-		</div>
-		<div class="mb-4">
-			<label for="password">Lösenordet som står i inbjudan</label>
-			<input
-				id="password"
-				placeholder="********"
-				name="password"
-				type="password"
-				value={form?.password ?? ''}
-				required
-			/>
-		</div>
-		<button class="send-response" type="submit">Skicka in</button>
-		{#if form?.success}
-			<div transition:fade class="message success">Tack för din anmälan {form.person}</div>
-		{/if}
-		{#if form?.wrongPassword}
-			<div transition:fade class="message password">Fel lösenord</div>
-		{/if}
-	</form>
-</section>
+				<input
+					id="email"
+					name="email"
+					placeholder="Email"
+					type="email"
+					value={form?.email ?? ''}
+					required
+				/>
+			</div>
+
+			<div class="mb-4">
+				<label for="foodPreference">Allergier eller matpreferenser</label>
+				<input
+					id="foodPreference"
+					placeholder="Allergier eller matpreferenser"
+					name="foodPreference"
+					type="text"
+					value={form?.foodPreference ?? ''}
+				/>
+			</div>
+			<div class="mb-4">
+				<label for="password">Lösenordet som står i inbjudan</label>
+				<input
+					id="password"
+					placeholder="********"
+					name="password"
+					type="password"
+					value={form?.password ?? ''}
+					required
+				/>
+			</div>
+			<button class="send-response" type="submit">Skicka in</button>
+			{#if form?.success}
+				<div transition:fade class="message success">Tack för din anmälan {form.person}</div>
+			{/if}
+			{#if form?.wrongPassword}
+				<div transition:fade class="message password">Fel lösenord</div>
+			{/if}
+		</form>
+	</section>
+{:else}
+	<section>
+		<h3>Ni kan tyvärr inte längre anmäla er.</h3>
+		<h3>Om Ni får förhinder kontakta Pétur eller Lisa via telefon eller SMS</h3>
+	</section>
+{/if}
 
 <style>
 	.message {
